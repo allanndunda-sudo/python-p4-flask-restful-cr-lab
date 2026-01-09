488d630d688b6dcd09c63625251b09641457617b
allanndunda <allanndunda>
2026-01-09 09:12:08 +0300

    created the table and routes endpoints

---
+++ server/migrations/versions/9873a810b7bb_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 9873a810b7bb
+Revises: 67f5d67aea55
+Create Date: 2026-01-09 08:01:10.492882
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9873a810b7bb'
+down_revision = '67f5d67aea55'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
